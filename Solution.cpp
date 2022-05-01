e6c92cdd589b6a2eafb3389793353a2a573267ea
Lachezar Kutsarov <lkutsarov@gmail.com>
2022-05-01 09:37:48 +0200

    Update Solution.cpp

--- Solution.cpp
+++ Solution.cpp
@@ -7,27 +7,28 @@ using namespace std;
 
 class Solution {
 
-	inline static unordered_map<string, function<int(int, int)>> operations{
-		{"+", [](int x, int y) {return x + y; }},
-		{"-", [](int x, int y) {return x - y; }},
-		{"*", [](int x, int y) {return x * y; }},
-		{"/", [](int x, int y) {return x / y; }}
-	};
-	
+    inline static unordered_map<string, function<int(int, int)>> operations
+    {
+	{"+", [](int x, int y) {return x + y; }},
+	{"-", [](int x, int y) {return x - y; }},
+	{"*", [](int x, int y) {return x * y; }},
+	{"/", [](int x, int y) {return x / y; }}
+    };
+
 public:
-	int evalRPN(vector<string>& tokens) {
-		deque<int> stack;
-		for (const auto& token : tokens) {
-			if (operations.find(token)== operations.end()) {
-				stack.push_back(stoi(token));
-				continue;
-			}
-			int secondOperand = stack.back();
-			stack.pop_back();
-			int firstOperand = stack.back();
-			stack.pop_back();
-			stack.push_back(operations[token](firstOperand, secondOperand));
-		}
-		return stack.back();
-	}
+    int evalRPN(vector<string>& tokens) {
+        deque<int> stack;
+        for (const auto& token : tokens) {
+            if (operations.find(token) == operations.end()) {
+                stack.push_back(stoi(token));
+                continue;
+            }
+            int secondOperand = stack.back();
+            stack.pop_back();
+            int firstOperand = stack.back();
+            stack.pop_back();
+            stack.push_back(operations[token](firstOperand, secondOperand));
+        }
+        return stack.back();
+    }
 };
